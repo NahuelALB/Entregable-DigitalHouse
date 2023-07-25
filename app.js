c9c82f13bd409b2e69673ecab5af6372d3b76cee
NahuelALB <ivanalbarado8@gmail.com>
2023-07-25 17:46:55 -0300

    Terminado la actividad de HTML del entregable de Mercado Liebre

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('public'));
+
+app.listen(3001, () => {
+  console.log('Servidor funcionando en el puerto 3001!');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/home.html'));
+});
