459e8cc545b323dce01f68c707fb33a54622ef27
NahuelALB <ivanalbarado8@gmail.com>
2023-08-09 00:09:12 -0300

    commit 3

--- app.js
+++ app.js
@@ -11,3 +11,7 @@ app.listen(3001, () => {
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './views/home.html'));
 });
+
+app.get('/registro', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/registro.html'));
+});
